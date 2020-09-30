b6a738430b92f6c5c062cf0bccae3a1065716ef0
yoyo <yoyo.zhou@cybex.io>
2020-09-30 12:27:39 +0800

    add test/runtime.dart

---
+++ test/runtime.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:convert/convert.dart';
+import 'package:scalecodec/scalecodec.dart';
+
+/// Load runtime data from file and init RuntimeConfiguration
+/// metadata.bin can be generated from tool/dump_metadata.dart
+void initRuntimeMetadata() {
+  File f = new File('metadata.bin');
+  String hex_str = hex.encode(f.readAsBytesSync());
+  createReaderInstance(hex_str);
+  getReaderInstance().read(4);//skip magic
+  dynamic metadata = fromBinary('MetadataEnum');
+  RuntimeConfigration().registMetadata(metadata);
+}
